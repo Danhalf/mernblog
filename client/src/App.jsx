c0e716c11f0bfd4d70c25fbf97f43a4c67da636e
Danhalf <danhel@rocketmail.com>
2023-05-19 19:13:17 +0300

    add: pages&components dir

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 export const App = () => (
     <>
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="text-3xl font-bold underline text-red-400">
             Hello world!
         </h1>
     </>
