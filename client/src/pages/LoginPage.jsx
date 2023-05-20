0aeaae80c46352f2e3c06e24e24be4613da7f4ff
Danhalf <danhel@rocketmail.com>
2023-05-20 09:26:55 +0300

    add: login form

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,21 @@
 export const LoginPage = () => (
     <>
-        <form className='w-1/4 h-1/2 bg-green-300' onSubmit={e=>e.preventDefault()}>
-
+        <form className='w-1/2 h-1/2 bg-transparent border-2 p-6 rounded-xl mx-auto mt-1/2' onSubmit={e=>e.preventDefault()}>
+            <h1 className="mb-4 text-lg text-white drop-shadow cursor-default text-center">Sign In</h1>
+            <label className='text-xs text-slate-400'>
+                Username
+                <input type="text"
+                placeholder='John Dou'
+                       className='text-center mt-1 text-black w-full rounded-lg bg-transparent border-2 py-1 px-2 outline-none placeholder:text-grey-400'
+                />
+            </label>
+            <label className='text-xs text-slate-400'>
+                Password
+                <input type="password"
+                       placeholder='**********'
+                       className='text-center mt-1 text-black w-full rounded-lg bg-transparent border-2 py-1 px-2 outline-none placeholder:text-grey-400'
+                />
+            </label>
         </form>
     </>
 );
